734453809615c7972ea5c17187930b9819c68795
Ned Batchelder <ned@nedbatchelder.com>
2025-03-19 11:00:09 -0400

    simple start

---
+++ script.py
@@ -0,0 +1,17 @@
+import sys
+
+print("=" * 80)
+print(f"{sys.version}")
+print(f"{sys.argv = }")
+
+print("sys.path:")
+for i, p in enumerate(sys.path):
+    print(f"{i}: {p}")
+
+print("")
+
+print("sys.flags:")
+for n in dir(sys.flags):
+    v = getattr(sys.flags, n)
+    if isinstance(v, (int, bool)):
+        print(f"{n}: {getattr(sys.flags, n)}")
